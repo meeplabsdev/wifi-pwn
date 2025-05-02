e1d053f6428776dca8d6660889673ad0d1a7a7e4
meeplabsdev <76091175+meeplabsdev@users.noreply.github.com>
2025-05-02 23:30:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+sudo sh -c "echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED65462EC8D5E4C5
+
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y iptables python3-pip python3-venv dnsmasq wget git aircrack-ng tcpdump tshark dnsmasq hostapd raspberrypi-kernel-headers git libgmp3-dev gawk qpdf bison flex make
+sudo apt install -y iptables python3-pip python3-venv dnsmasq wget git aircrack-ng tcpdump tshark dnsmasq hostapd raspberrypi-kernel-headers git libgmp3-dev gawk qpdf bison flex make mdk3 macchanger pwgen
+
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 if ! command -v docker >/dev/null 2>&1; then
